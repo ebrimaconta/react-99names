8803bc57dfa947f821ba3a8b13fa7f246ff65d7d
Ebrima Conta <ebrimaconta@gmail.com>
2019-05-21 14:46:45 +0100

    About to starting doing routers

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React from 'react';
 import './App.css';
 
 function Main() {
-    return ( <
+    return ( 
         <div className = "App" >
         <header className = "App-header" >
-
-        </header> </div >
+        The website is the 99 names by our beloved shaykh ibn uthymeen may Allaah be please with him and us may Allaah azza wal jaal reward the compile of the pdf may Allaah aid ahlul sunnah
+        </header>
+        <br />
+            <br /> Below is the pdf. May Allaah azza wal jal accept it
+            <br />
+        <a href="pdf/99names_new.pdf">
+          99 names of Allaah pdf
+          </a>
+             <a href="asmaquran.html">
+                <div class="circle-box">
+                    <div class="circle-outer">
+                        <div class="circle-inactive">
+                            Asma Ul Quran
+                        </div>
+                    </div>
+                </div>
+        </a>
+        </div >
     );
 }
 
