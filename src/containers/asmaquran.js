d9e51b036e034b039742ff31d36e26cd44d13a20
Ebrima Conta <ebrimaconta@gmail.com>
2020-09-25 09:00:20 +0100

    Remove tool tip

--- src/containers/asmaquran.js
+++ src/containers/asmaquran.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import ReactTooltip from "react-tooltip";
 
 import "../assets/css/style.css";
 import "../dist/style.css";
