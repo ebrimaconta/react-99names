89908d9ceb7ac7abe2a41ef63072a6d45546e77b
Ebrima Conta <ebrimaconta@gmail.com>
2021-01-16 21:18:06 +0000

    Added filter

--- src/domain/99names/99names.tsx
+++ src/domain/99names/99names.tsx
@@ -7,6 +7,8 @@ type AsmaListProps = {
 
 type IState = {
   names: any;
+  results: number;
+  updated: boolean;
 };
 
 class AsmaList extends Component<AsmaListProps, IState> {
@@ -14,37 +16,46 @@ class AsmaList extends Component<AsmaListProps, IState> {
     super(props);
     this.state = {
       names: this.props.Sort99names,
+      results: 0,
+      updated: false,
     };
   }
   componentDidUpdate(prevProps: AsmaListProps) {
     if (prevProps.Sort99names != this.props.Sort99names) {
-      console.log(this.props.Sort99names);
-      this.setState({ names: this.props.Sort99names });
+      this.setState({
+        names: this.props.Sort99names,
+        results: this.props.Sort99names.length,
+        updated: true,
+      });
     }
   }
 
   render() {
-    const { names } = this.state;
+    const { names, results, updated } = this.state;
     return (
       <>
-        <div className='flex  flex-wrap justify-center  my-10'>
-          {names.map((name: any, index: any) => {
-            return (
-              <div
-                className=' pattern-one w-lg-xs m-5 pt-10 md:w-1/3 '
-                key={index}
-              >
-                <div className='flex flex-col items-center py-10'>
-                  <div className='text-5xl'>{name.EnglishName} </div>
-                  <div className='text-4xl'> {name.ArabicName} </div>
-                  <div className='text-3xl text-center px-10 py-10'>
-                    {name.meaning}
+        <div className='flex  flex-col justify-center  my-10'>
+          <div className='text-center text-bl text-xl'>
+            {updated && `Number of names : ${results}`}
+          </div>
+          <div className='flex  flex-wrap justify-center  my-2'>
+            {names.map((name: any, index: any) => {
+              return (
+                <div
+                  className=' pattern-one w-lg-xs m-5 pt-10 md:w-1/3 '
+                  key={index}
+                >
+                  <div className='flex flex-col items-center py-10'>
+                    <div className='text-5xl'>{name.EnglishName} </div>
+                    <div className='text-4xl'> {name.ArabicName} </div>
+                    <div className='text-3xl text-center px-10 py-10'>
+                      {name.meaning}
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-          ;
+              );
+            })}
+          </div>
         </div>
       </>
     );
