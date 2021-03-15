d97501e36e1966df25d66f786d87014e58dc074b
Ebrima Conta <ebrimaconta@gmail.com>
2021-03-15 02:16:26 +0000

    Added docs

--- src/domain/Dashboard/Dashboard.tsx
+++ src/domain/Dashboard/Dashboard.tsx
@@ -5,11 +5,38 @@ import NameCard from '../AsmaQuran/NameCard';
 import { db } from '../../firebase/firebaseConfig';
 type IDashboard = {
   Sort99names: string[];
+  users: any;
 };
 function Dashboard(props: IDashboard) {
+  const [namesFromStore, setNames] = useState<string[]>([]);
+  db.collection('users')
+    .doc(`I9fQF4m9UNg2NNjWrLDuXihzrlr2`)
+    .get()
+    .then((doc: any) => {
+      setNames(doc.data().names);
+    });
+  console.log(props);
   return (
     <>
       <Header title='Dashboard' />
+      <div className='flex w-full justify-center'>
+        {props.Sort99names.map((names: any, index: any) => {
+          return (
+            <>
+              {namesFromStore.indexOf(index) > 1 && (
+                <NameCard
+                  key={index}
+                  english={names.EnglishName}
+                  arabic={names.ArabicName}
+                  meaning={names.meaning}
+                  references={names.reference}
+                  id={index}
+                />
+              )}
+            </>
+          );
+        })}
+      </div>
     </>
   );
 }
@@ -17,6 +44,7 @@ function Dashboard(props: IDashboard) {
 function mapStateToProps(state: any) {
   return {
     Sort99names: state.Sort99names,
+    users: state.users,
   };
 }
 
