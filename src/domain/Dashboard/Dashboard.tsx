31f68801c7989903a3f064e767b13e5f46f6bf3b
Ebrima Conta <ebrimaconta@gmail.com>
2021-03-15 12:36:41 +0000

    Added redirect

--- src/domain/Dashboard/Dashboard.tsx
+++ src/domain/Dashboard/Dashboard.tsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import NameCard from '../AsmaQuran/NameCard';
 import { db } from '../../firebase/firebaseConfig';
 import { getCookie } from '../../components/Cookie/Cookie';
+import { Redirect } from 'react-router';
 type IDashboard = {
   Sort99names: string[];
   users: any;
@@ -20,26 +21,30 @@ function Dashboard(props: IDashboard) {
   return (
     <>
       <Header title='Dashboard' />
-      <div className='flex justify-center'>
-        <div className='grid lg:grid-cols-3 grid-cols-1 sm:grid-cols-2'>
-          {props.Sort99names.map((names: any, index: any) => {
-            return (
-              <React.Fragment key={index}>
-                {namesFromStore.indexOf(index) !== -1 && (
-                  <NameCard
-                    key={index}
-                    english={names.EnglishName}
-                    arabic={names.ArabicName}
-                    meaning={names.meaning}
-                    references={names.reference}
-                    id={index}
-                  />
-                )}
-              </React.Fragment>
-            );
-          })}
+      {getCookie('uid') ? (
+        <div className='flex justify-center'>
+          <div className='grid lg:grid-cols-3 grid-cols-1 sm:grid-cols-2'>
+            {props.Sort99names.map((names: any, index: any) => {
+              return (
+                <React.Fragment key={index}>
+                  {namesFromStore.indexOf(index) !== -1 && (
+                    <NameCard
+                      key={index}
+                      english={names.EnglishName}
+                      arabic={names.ArabicName}
+                      meaning={names.meaning}
+                      references={names.reference}
+                      id={index}
+                    />
+                  )}
+                </React.Fragment>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      ) : (
+        <Redirect to='/' />
+      )}
     </>
   );
 }
